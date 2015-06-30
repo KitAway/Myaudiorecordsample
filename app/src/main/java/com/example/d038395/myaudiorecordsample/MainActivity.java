55a49eba59fb51cdc586415438a6eae9c9deac55
Truth <d038395@denethor.cdsdom.polito.it>
2015-06-30 17:33:32 +0200

    send
    Send audio file to server denthor

--- app/src/main/java/com/example/d038395/myaudiorecordsample/MainActivity.java
+++ app/src/main/java/com/example/d038395/myaudiorecordsample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.d038395.myaudiorecordsample;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +13,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.apache.http.client.methods.HttpPost;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,11 +36,15 @@ public class MainActivity extends ActionBarActivity {
     static MediaRecorder mRecorder=null;
     static MediaPlayer mPlayer=null;
     static final String LOG_TAG = "AudioRecordTest";
+    static final String filename="audio_record_test.mp4";
+    static final String targetURL="http://denethor.cdsdom.polito.it:9999";
+
+    static HttpURLConnection httpConn=null;
 
     public MainActivity(){
         super();
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
+        mFileName += "/"+filename;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +87,7 @@ public class MainActivity extends ActionBarActivity {
         else {
             mRecorder = new MediaRecorder();
             mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
             mRecorder.setOutputFile(mFileName);
             mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
             try {
@@ -112,5 +129,57 @@ public class MainActivity extends ActionBarActivity {
             textView.setText("Press to stop playing.");
             wPlayback =!wPlayback;
         }
+    }
+    public void onClickSend(View view) throws IOException{
+        final TextView textView = (TextView)findViewById(R.id.text_result);
+        UUID uuid=UUID.randomUUID();
+        File file= new File(mFileName);
+        long fileLength=file.length();
+        FileInputStream fis = new FileInputStream(file);
+        final byte[] buffer= new byte[(int)fileLength];
+        try{
+            fis.read(buffer);
+            URL url= new URL(targetURL);
+            httpConn=(HttpURLConnection)url.openConnection();
+            httpConn.setRequestMethod("POST");
+            httpConn.setRequestProperty("id", uuid.toString());
+            httpConn.setRequestProperty("audioname",filename);
+            httpConn.setRequestProperty("portBias", "0");
+            httpConn.setUseCaches(false);
+            httpConn.setDoInput(true);
+            httpConn.setDoOutput(true);
+            Thread thread = new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        //Your code goes here
+                        DataOutputStream wr =new DataOutputStream(httpConn.getOutputStream());
+                        wr.write(buffer);
+                        wr.close();
+
+                        InputStream is =httpConn.getInputStream();
+                        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while((line=rd.readLine())!=null) {
+                            response.append(line);
+                            response.append('\r');
+                        }
+                        rd.close();
+                        textView.setText(response.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
+
+        }
+        finally {
+            if(httpConn!=null)httpConn.disconnect();
+            fis.close();
+        }
+
     }
 }
